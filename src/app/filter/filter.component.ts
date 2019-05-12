b583f8950f9d93c2f40f160add13e04eb35de8f6
Paweł Matejko <matejko.pawel@gmail.com>
2019-05-12 18:11:10 +0200

    added prototype of filters component

--- src/app/filter/filter.component.ts
+++ src/app/filter/filter.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-filter',
@@ -6,8 +6,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./filter.component.scss']
 })
 export class FilterComponent implements OnInit {
-  // value = 'Clear me';
-  constructor() { }
+  dateFrom;
+  dateTo;
+  place = 'Chosen place';
+
+  ageFrom;
+  ageTo;
+  ageValues = Array.from({length: 100}, (v, k) => k + 1);
+  position;
+  nationality;
+
+  trainer;
+
+  activeFrom;
+  activeTo;
+  bestInSeason;
+  wonMatchesFrom;
+  wonMatchesTo;
+  lostMatchesFrom;
+  lostMatchesTo;
+  matchesValues = Array.from({length: 20}, (v, k) => k);
+
+  goalsNumberFrom;
+  goalsNumberTo;
+  goalsNumberValues = Array.from({length: 50}, (v, k) => k);
+  season;
+  redFlagsFrom;
+  redFlagsTo;
+  yellowFlagsFrom;
+  yellowFlagsTo;
+  flagsValues = Array.from({length: 20}, (v, k) => k);
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
