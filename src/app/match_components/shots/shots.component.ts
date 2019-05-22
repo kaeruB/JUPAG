8f95a09ab45afbd63b6a7e9878785c9caf4bae41
Agata Bogacz <agatabogacz@outlook.com>
2019-05-22 19:19:29 +0200

    Improving match screen.

--- src/app/match_components/shots/shots.component.ts
+++ src/app/match_components/shots/shots.component.ts
@@ -12,4 +12,14 @@ export class ShotsComponent implements OnInit {
   ngOnInit() {
   }
 
+  toggleTeam() {
+    const emptyGround1 = 'assets/img/half-ground.png';
+    const emptyGround2 = 'assets/img/half-ground-right.png';
+    const imgToToggle = document.querySelector('.empty-ground__img');
+    imgToToggle.setAttribute('src',  (imgToToggle.getAttribute('src') === emptyGround1) ? emptyGround2 : emptyGround1);
+    const headerToToggle = document.getElementsByClassName('shots__upper--header');
+    Array.from(headerToToggle).forEach((el) => {
+      el.classList.toggle('darken-header');
+    });
+  }
 }
