15f0dcf280d25173d578f917a63a2793bcde1f22
Agata Bogacz <agatabogacz@outlook.com>
2019-05-23 17:43:21 +0200

    Improving match screen.

--- src/app/match_components/shots/shots.component.ts
+++ src/app/match_components/shots/shots.component.ts
@@ -4,6 +4,8 @@ export interface Ball {
   id: string;
   left: number;
   top: number;
+  minute: number;
+  player: string;
 }
 
 @Component({
@@ -13,41 +15,56 @@ export interface Ball {
 })
 
 export class ShotsComponent implements OnInit {
-  balls: Ball[] = [
-    {id: 'ball1', left: 10, top: 20},
-    {id: 'ball2', left: 68, top: 40},
-    {id: 'ball3', left: 20, top: 40},
-    {id: 'ball4', left: 76, top: 56},
-    {id: 'ball5', left: 62, top: 20},
-    {id: 'ball6', left: 11, top: 35},
-    {id: 'ball7', left: 21, top: 21},
-    {id: 'ball8', left: 30, top: 50},
-    {id: 'ball9', left: 50, top: 17}
+  team1: Ball[] = [
+    {id: 'ball1', left: 10, top: 20, minute: 4, player: 'Sergio Ramos'},
+    {id: 'ball2', left: 68, top: 40, minute: 34, player: 'Sergio Ramos'},
+    {id: 'ball3', left: 20, top: 40, minute: 66, player: 'Sergio Ramos'},
+    {id: 'ball4', left: 76, top: 56, minute: 36, player: 'Sergio Ramos'},
+    {id: 'ball5', left: 62, top: 20, minute: 34, player: 'Sergio Ramos'},
+    {id: 'ball6', left: 11, top: 35, minute: 23, player: 'Sergio Ramos'},
+    {id: 'ball7', left: 21, top: 21, minute: 56, player: 'Sergio Ramos'},
+    {id: 'ball8', left: 30, top: 50, minute: 34, player: 'Sergio Ramos'},
+    {id: 'ball9', left: 50, top: 17, minute: 23, player: 'Sergio Ramos'}
   ];
-  balls2: Ball[] = [
-    {id: 'ball1', left: 15, top: 30},
-    {id: 'ball2', left: 23, top: 60},
-    {id: 'ball3', left: 67, top: 20},
-    {id: 'ball4', left: 73, top: 36},
-    {id: 'ball5', left: 26, top: 28},
-    {id: 'ball6', left: 16, top: 43},
-    {id: 'ball7', left: 24, top: 63},
-    {id: 'ball8', left: 76, top: 56},
-    {id: 'ball9', left: 64, top: 25}
+  team2: Ball[] = [
+    {id: 'ball1', left: 15, top: 30, minute: 65, player: 'Lionel Messi'},
+    {id: 'ball2', left: 23, top: 60, minute: 23, player: 'Lionel Messi'},
+    {id: 'ball3', left: 67, top: 20, minute: 54, player: 'Lionel Messi'},
+    {id: 'ball4', left: 73, top: 36, minute: 12, player: 'Lionel Messi'},
+    {id: 'ball5', left: 26, top: 28, minute: 2, player: 'Lionel Messi'},
+    {id: 'ball6', left: 16, top: 43, minute: 45, player: 'Lionel Messi'},
+    {id: 'ball7', left: 24, top: 63, minute: 23, player: 'Lionel Messi'},
+    {id: 'ball8', left: 76, top: 56, minute: 35, player: 'Lionel Messi'},
+    {id: 'ball9', left: 64, top: 25, minute: 50, player: 'Lionel Messi'}
   ];
-  currentlyUsedBallsPositions = 'balls';
+  currentlyUsedBallsPositions = 'team1';
 
   constructor() { }
 
   ngOnInit() {
-    this.loadBallsPositions(this.balls);
+    this.setBallsPositionsOnGround(this.team1);
+    this.setBallsPositionsOnTimeline(this.team1, this.team2);
   }
 
-  loadBallsPositions(ballsPositionsArray) {
+  setBallsPositionsOnGround(ballsPositionsArray) {
     ballsPositionsArray.forEach( e => {
       const newBallIcon = document.getElementById(e.id);
       newBallIcon.style.top = e.top + '%';
       newBallIcon.style.left = e.left + '%';
+    });
+  }
+
+  setBallsPositionsOnTimeline(team1, team2) {
+    team1.forEach( e => {
+      const newBallIconOnTimelineTeam1 = document.getElementById(e.id + '-timeline-team1');
+      newBallIconOnTimelineTeam1.style.top = e.minute + '%';
+      newBallIconOnTimelineTeam1.style.left = '0%';
+    });
+
+    team2.forEach( e => {
+      const newBallIconOnTimelineTeam2 = document.getElementById(e.id + '-timeline-team2');
+      newBallIconOnTimelineTeam2.style.top = e.minute + '%';
+      newBallIconOnTimelineTeam2.style.left = '45%';
     });
   }
 
@@ -62,16 +79,26 @@ export class ShotsComponent implements OnInit {
       el.classList.toggle('darken-header');
     });
 
-    if (this.currentlyUsedBallsPositions === 'balls') {
-      this.currentlyUsedBallsPositions = 'balls2';
-      this.loadBallsPositions(this.balls2);
+    if (this.currentlyUsedBallsPositions === 'team1') {
+      this.currentlyUsedBallsPositions = 'team2';
+      this.setBallsPositionsOnGround(this.team2);
     } else {
-      this.loadBallsPositions(this.balls);
-      this.currentlyUsedBallsPositions = 'balls';
+      this.setBallsPositionsOnGround(this.team1);
+      this.currentlyUsedBallsPositions = 'team1';
     }
 
     this.removeBallSelection();
+
+    this.toggleTimeline();
   }
+
+  toggleTimeline() {
+    const timelines = document.getElementsByClassName('shots__timeline');
+    Array.from(timelines).forEach((el) => {
+      el.classList.toggle('hidden');
+    });
+  }
+
   ballOnClick($event) {
     this.removeBallSelection();
     $event.currentTarget.firstChild.firstChild.classList.add('ball-chosen');
