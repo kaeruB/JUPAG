f98af06da8d5005d1d20863bac08a5ce18d69acf
justynamaciag <justynamaciag96@gmail.com>
2019-05-24 20:38:03 +0200

    Add routing in header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,9 @@ const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'search', component: SearchComponent },
-  { path: 'match', component: MatchComponent }
+  { path: 'match', component: MatchComponent },
+  { path: 'player', component: HomeComponent },
+  { path: 'team', component: HomeComponent }
 ];
 
 @NgModule({
