8cf7c0b52267b7012f8272aa589089ef491dc75d
Agata Bogacz <agatabogacz@outlook.com>
2019-06-11 07:22:43 +0200

    Filter improvements

--- src/app/filter/filter.component.ts
+++ src/app/filter/filter.component.ts
@@ -8,7 +8,7 @@ import {Component, OnInit} from '@angular/core';
 export class FilterComponent implements OnInit {
   dateFrom;
   dateTo;
-  place = 'Chosen place';
+  place;
 
   ageFrom;
   ageTo;
