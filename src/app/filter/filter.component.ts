8c5847ae039642241a1b04e70d403a1966aa405d
justynamaciag <justynamaciag96@gmail.com>
2019-06-13 04:24:55 +0200

    Validation order working

--- src/app/filter/filter.component.ts
+++ src/app/filter/filter.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-filter',
@@ -6,6 +7,28 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./filter.component.scss']
 })
 export class FilterComponent implements OnInit {
+  constructor(
+    private builder: FormBuilder) {
+    this.theFormGroup = this.builder.group({
+      dateFrom: ['', []],
+      dateTo: ['', []],
+      ageFrom: ['', []],
+      ageTo: ['', []],
+      wonMatchesFrom: ['', []],
+      wonMatchesTo: ['', []],
+      activeFrom: ['', []],
+      activeTo: ['', []],
+      lostMatchesFrom: ['', []],
+      lostMatchesTo: ['', []],
+      goalsNumberFrom: ['', []],
+      goalsNumberTo: ['', []],
+      yellowFlagsFrom: ['', []],
+      yellowFlagsTo: ['', []],
+      redFlagsFrom: ['', []],
+      redFlagsTo: ['', []],
+    });
+  }
+  theFormGroup: FormGroup;
   dateFrom;
   dateTo;
   place = 'Chosen place';
@@ -36,10 +59,70 @@ export class FilterComponent implements OnInit {
   yellowFlagsFrom;
   yellowFlagsTo;
   flagsValues = Array.from({length: 20}, (v, k) => k);
-
-  constructor() {
-  }
-
   ngOnInit() {
   }
+  submit(): void {
+    this.theFormGroup.updateValueAndValidity();
+
+    this.theFormGroup.get('dateFrom').updateValueAndValidity();
+    this.theFormGroup.get('dateTo').updateValueAndValidity();
+
+    this.theFormGroup.get('activeFrom').updateValueAndValidity();
+    this.theFormGroup.get('activeTo').updateValueAndValidity();
+
+    this.theFormGroup.get('ageFrom').updateValueAndValidity();
+    this.theFormGroup.get('ageTo').updateValueAndValidity();
+
+    this.theFormGroup.get('wonMatchesFrom').updateValueAndValidity();
+    this.theFormGroup.get('wonMatchesTo').updateValueAndValidity();
+    this.theFormGroup.get('lostMatchesFrom').updateValueAndValidity();
+    this.theFormGroup.get('lostMatchesTo').updateValueAndValidity();
+    this.theFormGroup.get('goalsNumberFrom').updateValueAndValidity();
+    this.theFormGroup.get('goalsNumberTo').updateValueAndValidity();
+
+    this.theFormGroup.get('yellowFlagsFrom').updateValueAndValidity();
+    this.theFormGroup.get('yellowFlagsTo').updateValueAndValidity();
+
+    this.theFormGroup.get('redFlagsFrom').updateValueAndValidity();
+    this.theFormGroup.get('redFlagsTo').updateValueAndValidity();
+    this.theFormGroup.setValidators(this.checkOrder);
+    this.theFormGroup.updateValueAndValidity();
+
+    if (this.theFormGroup.valid) {
+      console.log('Form is valid');
+    } else {
+      console.log('There is still an error in the form');
+    }
+  }
+
+  checkOrder(group: FormGroup): any {
+    if (group) {
+      if (group.get('dateFrom').value > group.get('dateTo').value) {
+        return {dateNotOrdered : true};
+      }
+      if (group.get('activeFrom').value > group.get('activeTo').value) {
+        return {activeDateNotOrdered : true};
+      }
+      if (group.get('ageFrom').value > group.get('ageTo').value) {
+        return {ageNotOrdered : true};
+      }
+      if (group.get('wonMatchesFrom').value > group.get('wonMatchesTo').value) {
+        return {wonMatchesNotOrdered : true};
+      }
+      if (group.get('lostMatchesFrom').value > group.get('lostMatchesTo').value) {
+        return {lostMatchesNotOrdered : true};
+      }
+      if (group.get('goalsNumberFrom').value > group.get('goalsNumberTo').value) {
+        return {goalsNumberNotOrdered : true};
+      }
+      if (group.get('yellowFlagsFrom').value > group.get('yellowFlagsTo').value) {
+        return {yellowFlagsNotOrdered : true};
+      }
+      if (group.get('redFlagsFrom').value > group.get('redFlagsTo').value) {
+        return {redFlagsNotOrdered : true};
+      }
+    }
+    return null;
+  }
+
 }
